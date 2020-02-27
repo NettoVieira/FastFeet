e79a8f4e42c8cedd16adf8795e63a167538bfc9b
NettoVieira <joao.fvneto@hotmail.com>
2020-02-27 06:24:24 -0300

    configurando eslint mais as estruturas de pastas junto o sequelize

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ messsage: 'Hello Word'});
-})
+routes.get('/', (req, res) => res.json({ messsage: 'Hello Word' }));
 
 export default routes;
