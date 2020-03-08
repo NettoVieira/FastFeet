818a72b01372e57cfc00cc25693acd04a4b4d017
NettoVieira <joao.fvneto@hotmail.com>
2020-03-08 15:00:47 -0300

    Criando migration e seeds de usuarios junto com a model de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => res.json({ messsage: 'Hello Word' }));
+routes.post('/users', UserController.store);
 
 export default routes;
